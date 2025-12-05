eeef9fa83e2ae34bc5d96c6f0ff1b72ad843c727
Zhan Isaakian <jeanisahakyan@gmail.com>
2025-12-04 23:00:22 -0800

    chore(landing): paths fixes

--- apps/landing/vite.config.ts
+++ apps/landing/vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: './',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
